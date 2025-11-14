e4e79679da69a22bd5032a0d02609f17c548b3e0
saby75 <saby75@gmail.com>
2025-11-14 17:44:28 +0000

    fix: Systematically correct all path aliases and imports for root structure

--- components/ModuleContent.tsx
+++ components/ModuleContent.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useSearchParams } from 'next/navigation';
-import { courseData } from '../data/course-data';
+import { courseData } from '@/data/course-data';
 import Link from 'next/link';
 import { Suspense } from 'react';
 
@@ -47,7 +47,6 @@ function ModuleDisplay() {
   );
 }
 
-// This is the component we will export and wrap in Suspense
 export function ModuleContent() {
     return (
         <Suspense fallback={<div>Loading module...</div>}>
